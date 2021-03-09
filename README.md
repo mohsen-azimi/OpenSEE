f3626ee4df4a065e74580be633eef09b877ac8d4
Mohsen Azimi <24981663+mohsen-azimi@users.noreply.github.com>
2021-03-08 17:18:27 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# OpenSEE
+An Open-source Python Package for Structural & Earthquake Engineering.
+
+## Installation
+```pip install opensee
+
+```
+
+## How to use it?
+Open terminal and type opensee.
+
+```Under Cinstruction!```
+
+## License
+
+© 2021 Mohsen Azimi
+
+This repository is licensed under the MIT license. See LICENSE for details.
---
+++ setup.py
@@ -0,0 +1,29 @@
+import pathlib
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+# This call to setup() does all the work
+setuptools.setup(
+   name='opensee',
+   version='0.0.1',
+   author='Mohsen Azimi',
+   author_email='mohsen.azimi@ubc.ca',
+   license='MIT',
+   description="An Open-source Package for Structural & Earthquake Engineering",
+   long_description=long_description,
+   long_description_content_type="text/markdown",
+   url='https://github.com/mohsen-azimi/OpenSEE',
+   packages=setuptools.find_packages(),
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+   include_package_data=True,
+   install_requires=[],
+   python_requires='>=3.8',
+
+)
